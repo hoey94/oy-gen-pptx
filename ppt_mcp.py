2e8e13739d8d72017d724ee15af1970ec200e17f
hoey94 <cnnqjban521@gmail.com>
2025-10-21 18:23:50 +0800

    add: 下载ppt服务

--- ppt_mcp.py
+++ ppt_mcp.py
@@ -6,7 +6,8 @@ mcp = FastMCP("生成pptx mcp", host='172.22.61.12', port=8001)
 @mcp.tool()
 def add(pptx_json_str,output_file_name) -> int:
     """根据pptx和json内容生成ppt"""
-    return app.generate_pptx(pptx_json_str,output_file_name)
+    app.generate_pptx(pptx_json_str,output_file_name)
+    return f'下载路径: 172.22.61.12:8000/{output_file_name}'
 
 if __name__ == "__main__":
     mcp.run(transport='sse')
